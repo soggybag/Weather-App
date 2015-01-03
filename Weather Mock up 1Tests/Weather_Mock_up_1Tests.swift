4ee4d278c267478394ebddb2ee5c95128c28fac0
Mitchell Hudson <soggybag@gmail.com>
2015-01-03 00:10:31 -0800

    Initial Commit

---
+++ Weather Mock up 1Tests/Weather_Mock_up_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Weather_Mock_up_1Tests.swift
+//  Weather Mock up 1Tests
+//
+//  Created by mitchell hudson on 1/3/15.
+//  Copyright (c) 2015 mitchell hudson. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Weather_Mock_up_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
