8a1ecbf6cf618467f6fa121c286e27c065749292
Mitchell Hudson <soggybag@gmail.com>
2015-01-12 15:18:09 -0800

    First commit

---
+++ Weather Mock up 1/OpenWeatherMap.swift
@@ -0,0 +1,172 @@
+//
+//  OpenWeatherMap.swift
+//  Weather Mock up 1
+//
+//  Created by mitchell hudson on 1/8/15.
+//  Copyright (c) 2015 mitchell hudson. All rights reserved.
+//
+
+// City Not found returns:
+// {"message":"Error: Not found city","cod":"404"}
+
+
+import UIKit
+
+protocol OpenWeatherMapDelegate {
+    func updateWeather()
+}
+
+class OpenWeatherMap {
+    
+    // Delegate that will display the weather
+    var delegate: OpenWeatherMapDelegate!
+    
+    // Define some constants to hold info that won't change
+    var openWeatherAPIKey = "0"
+    let openWeatherURL = "http://api.openweathermap.org/data/2.5/weather"
+    
+    // Variables used by the program
+    var cityName            = "San Francisco"
+    var weatherDescription  = ""
+    var weatherTemp         = ""
+    var weatherMinTemp      = ""
+    var weatherMaxTemp      = ""
+    var weatherDateString   = ""
+    var weatherWindDeg      = ""
+    var weatherWindSpeed    = ""
+    var weatherImageName    = ""
+    
+    
+    // MARK: - Initializer
+    init() {
+        
+    }
+    
+    
+    // MARK: - Load Weather
+    
+    // Load Weather 
+    func loadWeather() {
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // APPID=APIKEY
+        // q=city name
+        
+        var request = HTTPTask()
+        request.requestSerializer = JSONRequestSerializer()
+        request.responseSerializer = JSONResponseSerializer()
+        
+        request.GET(openWeatherURL, parameters: ["q":self.cityName, "APPID":openWeatherAPIKey], success: {(response: HTTPResponse) in
+            // Wrap the dict with swifty
+            if let dict = response.responseObject as? Dictionary<String, AnyObject> {
+                let json = JSON(object: dict)
+                
+                // Get the weather description
+                if let description = json["weather"][0]["description"].stringValue{
+                    self.weatherDescription = description
+                }
+                
+                // The temperature
+                if let temp = json["main"]["temp"].doubleValue {
+                    self.weatherTemp = "\(self.fixTempForDisplay(temp))°"
+                }
+                
+                // The minimum temperature
+                if let minTemp = json["main"]["temp_min"].doubleValue {
+                    self.weatherMinTemp = "\(self.fixTempForDisplay(minTemp))"
+                }
+                
+                // The maximum temperature
+                if let maxTemp = json["main"]["temp_max"].doubleValue {
+                    self.weatherMaxTemp = "\(self.fixTempForDisplay(maxTemp))"
+                }
+                
+                // City name
+                if let city = json["name"].stringValue {
+                    self.cityName = city
+                }
+                
+                if let dateTime = json["dt"].doubleValue {
+                    let date = NSDate(timeIntervalSince1970: dateTime)
+                    self.weatherDateString = self.formatDate(date)
+                }
+                
+                if let windDeg = json["wind"]["deg"].doubleValue {
+                    self.weatherWindDeg = "\(windDeg)"
+                }
+                
+                if let windSpeed = json["wind"]["speed"].doubleValue {
+                    self.weatherWindSpeed = "\(windSpeed)"
+                }
+                
+                if let weatherImage = json["weather"][0]["icon"].stringValue {
+                    self.weatherImageName = weatherImage
+                }
+                
+                
+                // Network session handled on separate thread, sync this with the main thread to update labels
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.delegate.updateWeather()
+                })
+            }
+            
+            }, failure: {(error: NSError, repsonse: HTTPResponse?) in
+                println("error \(error)")
+        })
+        
+    }
+    
+    
+    // MARK: - Helper functions
+    func timeStampToDate(timeStamp: NSTimeInterval) -> NSDate {
+        return NSDate(timeIntervalSince1970: timeStamp)
+    }
+    
+    
+    func formatDate(date: NSDate) -> String {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.FullStyle
+        return dateFormatter.stringFromDate(date)
+    }
+    
+    
+    func cToF(tempC: Double) -> Double {
+        return (tempC * 1.8) + 32
+    }
+    
+    func fToC(tempF: Double) -> Double {
+        return (tempF - 32) / 1.8
+    }
+    
+    
+    func kelvinToDegrees(tempK: Double) -> Double {
+        return tempK - 272.15
+    }
+    
+    
+    func fixTempForDisplay(temp: Double) -> String {
+        
+        println("Kelvin: \(temp)")
+        println("C: \(temp - 273.15)")
+        
+        let tempC = kelvinToDegrees(temp)
+        let tempF = cToF(tempC)
+        let tempR = round(tempF)
+        let tempString = String(format: "%.0f", tempR)
+        return tempString
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Weather Mock up 1/ViewController.swift
+++ Weather Mock up 1/ViewController.swift
@@ -6,18 +6,111 @@
 //  Copyright (c) 2015 mitchell hudson. All rights reserved.
 //
 
+
+// Todo:
+// Need night images for weather icons
+
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, OpenWeatherMapDelegate {
+    
+    // MARK: - Variables
+    
+    var openWeatherMap = OpenWeatherMap()
+    
+    // Define some constants to hold info that won't change
+    let openWeatherAPIKey = "2854c5771899ff92cd962dd7ad58e7b0"
+    let openWeatherURL = "http://api.openweathermap.org/data/2.5/weather"
+    
+    
+    // MARK: - IBOutlets
+    
+    // IBOutlets
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var weatherImage: UIImageView!
+    
+    // MARK: - IBActions
+    
+    // IBActions 
+    @IBAction func cityButtonPressed(sender: AnyObject) {
+        openAlertBox()
+    }
+    
+    
+    // MARK: View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set self as delegate for open weather map
+        self.openWeatherMap.delegate = self
+        self.openWeatherMap.cityName = "San Francisco"
+        self.openWeatherMap.openWeatherAPIKey = self.openWeatherAPIKey
+        self.openWeatherMap.loadWeather()
+        
+        self.openWeatherMap.loadWeather()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - City Name Alert
+    
+    // This function opens an alert box with two buttons and an input field
+    func openAlertBox() {
+        // Make a UIAlertController
+        let alert = UIAlertController(title: "City", message: "Enter city name", preferredStyle: .Alert)
+        
+        // Make a cancel action
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) -> Void in
+            // The cancel action could do something here
+        }
+        // Add the cancel action
+        alert.addAction(cancelAction)
+        
+        // Make an OK action. This will look at the textfield in the alert.
+        let okAction = UIAlertAction(title: "OK", style: .Default) { (action) -> Void in
+            // Choosing OK will set the city name
+            if let cityTextField: UITextField = alert.textFields?.first as? UITextField {
+                self.openWeatherMap.cityName = cityTextField.text
+                self.openWeatherMap.loadWeather()
+            }
+        }
+        // Add the OK action to the Alert
+        alert.addAction(okAction)
+        
+        // Add a text field to the alert
+        alert.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "city"
+        }
+        
+        // Present the alert box.
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - Update Weather 
+    
+    func updateWeather() {
+        cityLabel.text          = self.openWeatherMap.cityName
+        descriptionLabel.text   = self.openWeatherMap.weatherDescription
+        tempLabel.text          = self.openWeatherMap.weatherTemp
+        minTempLabel.text       = self.openWeatherMap.weatherMinTemp
+        maxTempLabel.text       = self.openWeatherMap.weatherMaxTemp
+        dateLabel.text          = self.openWeatherMap.weatherDateString
+        weatherImage.image      = UIImage(named: self.openWeatherMap.weatherImageName)
+        weatherImage.contentMode = UIViewContentMode.ScaleAspectFill
     }
 
 
